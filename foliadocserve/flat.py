d0a5cb7d83a6e62502b190c7df4827c2dd33ba36
Maarten van Gompel <proycon@anaproy.nl>
2015-02-23 12:23:48 +0100

    restructuring

---
+++ foliadocserve/flat.py
@@ -0,0 +1,158 @@
+def parseresults(results):
+    raise NotImplementedError
+
+def gethtml(element):
+    """Converts the element to html skeleton"""
+    if isinstance(element, folia.Correction):
+        s = ""
+        if element.hasnew():
+            for child in element.new():
+                if isinstance(child, folia.AbstractStructureElement) or isinstance(child, folia.Correction):
+                    s += gethtml(child)
+        elif element.hascurrent():
+            for child in element.current():
+                if isinstance(child, folia.AbstractStructureElement) or isinstance(child, folia.Correction):
+                    s += gethtml(child)
+        return s
+    elif isinstance(element, folia.AbstractStructureElement):
+        s = ""
+        for child in element:
+            if isinstance(child, folia.AbstractStructureElement) or isinstance(child, folia.Correction):
+                s += gethtml(child)
+        if not isinstance(element, folia.Text) and not isinstance(element, folia.Division):
+            try:
+                label = "<span class=\"lbl\">" + element.text() + "</span>"
+            except folia.NoSuchText:
+                label = "<span class=\"lbl\"></span>"
+        else:
+            label = ""
+        if not isinstance(element,folia.Word) or (isinstance(element, folia.Word) and element.space):
+            label += " "
+
+        if not element.id:
+            element.id = element.doc.id + "." + element.XMLTAG + ".id" + str(random.randint(1000,999999999))
+        if s:
+            s = "<div id=\"" + element.id + "\" class=\"F " + element.XMLTAG + "\">" + label + s
+        else:
+            s = "<div id=\"" + element.id + "\" class=\"F " + element.XMLTAG + " deepest\">" + label
+        if isinstance(element, folia.Linebreak):
+            s += "<br />"
+        if isinstance(element, folia.Whitespace):
+            s += "<br /><br />"
+        elif isinstance(element, folia.Figure):
+            s += "<img src=\"" + element.src + "\">"
+        s += "</div>"
+        if isinstance(element, folia.List):
+            s = "<ul>" + s + "</ul>"
+        elif isinstance(element, folia.ListItem):
+            s = "<li>" + s + "</li>"
+        elif isinstance(element, folia.Table):
+            s = "<table>" + s + "</table>"
+        elif isinstance(element, folia.Row):
+            s = "<tr>" + s + "</tr>"
+        elif isinstance(element, folia.Cell):
+            s = "<td>" + s + "</td>"
+        return s
+    else:
+        raise Exception("Structure element expected, got " + str(type(element)))
+
+def getannotations(element, previouswordid = None):
+    if isinstance(element, folia.Correction):
+        if not element.id:
+            #annotator requires IDS on corrections, make one on the fly
+            hash = random.getrandbits(128)
+            element.id = element.doc.id + ".correction.%032x" % hash
+        correction_new = []
+        correction_current = []
+        correction_original = []
+        correction_suggestions = []
+        if element.hasnew():
+            for x in element.new():
+                for y in  getannotations(x):
+                    if not 'incorrection' in y: y['incorrection'] = []
+                    y['incorrection'].append(element.id)
+                    correction_new.append(y)
+                    yield y #yield as any other
+        if element.hascurrent():
+            for x in element.current():
+                for y in  getannotations(x):
+                    if not 'incorrection' in y: y['incorrection'] = []
+                    y['incorrection'].append(element.id)
+                    correction_current.append(y)
+                    yield y #yield as any other
+        if element.hasoriginal():
+            for x in element.original():
+                for y in  getannotations(x):
+                    y['auth'] = False
+                    if not 'incorrection' in y: y['incorrection'] = []
+                    y['incorrection'].append(element.id)
+                    correction_original.append(y)
+        if element.hassuggestions():
+            for x in element.suggestions():
+                for y in  getannotations(x):
+                    y['auth'] = False
+                    if not 'incorrection' in y: y['incorrection'] = []
+                    y['incorrection'].append(element.id)
+                    correction_suggestions.append(y)
+
+        annotation = {'id': element.id ,'set': element.set, 'class': element.cls, 'type': 'correction', 'new': correction_new,'current': correction_current, 'original': correction_original, 'suggestions': correction_suggestions}
+        if element.annotator:
+            annotation['annotator'] = element.annotator
+        if element.annotatortype == folia.AnnotatorType.AUTO:
+            annotation['annotatortype'] = "auto"
+        elif element.annotatortype == folia.AnnotatorType.MANUAL:
+            annotation['annotatortype'] = "manual"
+        p = element.ancestor(folia.AbstractStructureElement)
+        annotation['targets'] = [ p.id ]
+        yield annotation
+    elif isinstance(element, folia.AbstractTokenAnnotation) or isinstance(element,folia.TextContent):
+        annotation = element.json()
+        p = element.parent
+        #log("Parent of " + str(repr(element))+ " is "+ str(repr(p)))
+        p = element.ancestor(folia.AbstractStructureElement)
+        annotation['targets'] = [ p.id ]
+        assert isinstance(annotation, dict)
+        yield annotation
+    elif isinstance(element, folia.AbstractSpanAnnotation):
+        if not element.id and (folia.Attrib.ID in element.REQUIRED_ATTRIBS or folia.Attrib.ID in element.OPTIONAL_ATTRIBS):
+            #span annotation elements must have an ID for the editor to work with them, let's autogenerate one:
+            element.id = element.doc.data[0].generate_id(element)
+            #and add to index
+            element.doc.index[element.id] = element
+        annotation = element.json()
+        annotation['span'] = True
+        annotation['targets'] = [ x.id for x in element.wrefs() ]
+        assert isinstance(annotation, dict)
+        yield annotation
+    if isinstance(element, folia.AbstractStructureElement):
+        annotation =  element.json(None, False) #no recursion
+        annotation['self'] = True #this describes the structure element itself rather than an annotation under it
+        annotation['targets'] = [ element.id ]
+        yield annotation
+    if isinstance(element, folia.AbstractStructureElement) or isinstance(element, folia.AbstractAnnotationLayer) or isinstance(element, folia.AbstractSpanAnnotation) or isinstance(element, folia.Suggestion):
+        for child in element:
+            for x in getannotations(child, previouswordid):
+                assert isinstance(x, dict)
+                if previouswordid and not 'previousword' in x:
+                    x['previousword'] = previouswordid
+                yield x
+            if isinstance(child, folia.Word):
+                previouswordid = child.id
+
+def getdeclarations(doc):
+    for annotationtype, set in doc.annotations:
+        try:
+            C = folia.ANNOTATIONTYPE2CLASS[annotationtype]
+        except KeyError:
+            pass
+        #if (issubclass(C, folia.AbstractAnnotation) or C is folia.TextContent or C is folia.Correction) and not (issubclass(C, folia.AbstractTextMarkup)): #rules out structure elements for now
+        if not issubclass(C, folia.AbstractTextMarkup) and annotationtype in folia.ANNOTATIONTYPE2XML:
+            annotationtype = folia.ANNOTATIONTYPE2XML[annotationtype]
+            yield {'annotationtype': annotationtype, 'set': set}
+
+def getsetdefinitions(doc):
+    setdefs = {}
+    for annotationtype, set in doc.annotations:
+        if set in doc.setdefinitions:
+            setdefs[set] = doc.setdefinitions[set].json()
+    return setdefs
---
+++ foliadocserve/test.py
@@ -0,0 +1,98 @@
+def testequal(value, reference, testmessage,testresult=True):
+    if value == reference:
+        testmessage = testmessage + ": Ok!\n"
+        if testresult:
+            testresult = True
+    else:
+        testmessage = testmessage + ": Failed! Value \"" + str(value) + "\" does not match reference \"" + str(reference) + "\"\n"
+        testresult = False
+    return testresult, testmessage
+
+def test(doc, testname, testmessage = ""):
+    log("Running test " + testname)
+
+    #load clean document
+    #perform test
+    testresult = True #must start as True for chaining
+    try:
+        if testname in ( "textchange", "correction_textchange"):
+            testresult, testmessage = testequal(doc['untitleddoc.p.3.s.1.w.2'].text(),"mijn", testmessage + "Testing text", testresult)
+        elif testname in ( "textmerge","correction_textmerge"):
+            testresult, testmessage = testequal(doc['untitleddoc.p.3.s.1.w.14'].text(),"wegreden", testmessage + "Testing text", testresult)
+        elif testname in ("multiannotchange"):
+            testresult, testmessage = testequal(doc['untitleddoc.p.3.s.6.w.8'].text(),"het", testmessage + "Testing text", testresult)
+            testresult, testmessage = testequal(doc['untitleddoc.p.3.s.6.w.8'].pos(),"LID(onbep,stan,rest)", testmessage + "Testing pos class", testresult)
+            testresult, testmessage = testequal(doc['untitleddoc.p.3.s.6.w.8'].lemma(),"het", testmessage + "Testing lemma class", testresult)
+        elif testname in ("correction_tokenannotationchange"):
+            testresult, testmessage = testequal(doc['untitleddoc.p.3.s.6.w.8'].pos(),"LID(onbep,stan,rest)", testmessage + "Testing pos class", testresult)
+        elif testname in ("addentity", "correction_addentity"):
+            testresult, testmessage = testequal(doc['untitleddoc.p.3.s.1.entity.1'].cls,"per", testmessage + "Testing presence of new entity", testresult)
+            testresult, testmessage = testequal(len(doc['untitleddoc.p.3.s.1.entity.1'].wrefs()),2, testmessage + "Testing span size", testresult)
+            testresult, testmessage = testequal(doc['untitleddoc.p.3.s.1.entity.1'].wrefs(0).id, 'untitleddoc.p.3.s.1.w.12' , testmessage + "Testing order (1/2)", testresult)
+            testresult, testmessage = testequal(doc['untitleddoc.p.3.s.1.entity.1'].wrefs(1).id, 'untitleddoc.p.3.s.1.w.12b' , testmessage + "Testing order (2/2)", testresult)
+        elif testname in  ("worddelete"):
+            testresult, testmessage = testequal('untitleddoc.p.3.s.8.w.10' in doc,False, testmessage + "Testing absence of word in index", testresult)
+        elif testname in ( "wordsplit"):
+            testresult, testmessage = testequal('untitleddoc.p.3.s.12.w.5' in doc,False, testmessage + "Testing absence of original word in index", testresult)
+            testresult, testmessage = testequal(doc['untitleddoc.p.3.s.12.w.18'].text(),"4", testmessage + "Testing new word (1/2)", testresult)
+            testresult, testmessage = testequal(doc['untitleddoc.p.3.s.12.w.17'].text(),"uur", testmessage + "Testing new word (2/2)", testresult)
+            testresult, testmessage = testequal(doc['untitleddoc.p.3.s.12.w.18'].next().id,"untitleddoc.p.3.s.12.w.17", testmessage + "Testing order (1/2)", testresult)
+            testresult, testmessage = testequal(doc['untitleddoc.p.3.s.12.w.4'].next().id,"untitleddoc.p.3.s.12.w.18", testmessage + "Testing order (2/2)", testresult)
+        elif testname in ("wordinsertionright", "correction_wordinsertionright"):
+            testresult, testmessage = testequal(doc['untitleddoc.p.3.s.12.w.1'].text(),"en", testmessage + "Testing original word", testresult)
+            testresult, testmessage = testequal(doc['untitleddoc.p.3.s.12.w.17'].text(),"we", testmessage + "Testing new word", testresult)
+            testresult, testmessage = testequal(doc['untitleddoc.p.3.s.12.w.1'].next().id,"untitleddoc.p.3.s.12.w.17", testmessage + "Testing order", testresult)
+        elif testname in ("wordinsertionleft", "correction_wordinsertionleft"):
+            testresult, testmessage = testequal(doc['untitleddoc.p.3.s.13.w.12'].text(),"hoorden", testmessage + "Testing original word", testresult)
+            testresult, testmessage = testequal(doc['untitleddoc.p.3.s.13.w.16'].text(),"we", testmessage + "Testing new word", testresult)
+            testresult, testmessage = testequal(doc['untitleddoc.p.3.s.13.w.16'].next().id,"untitleddoc.p.3.s.13.w.12", testmessage + "Testing order", testresult)
+        elif testname in ("spanchange"):
+            testresult, testmessage = testequal(len(doc['untitleddoc.p.3.s.9.entity.1'].wrefs()),3, testmessage + "Testing span size", testresult)
+            testresult, testmessage = testequal(doc['untitleddoc.p.3.s.9.entity.1'].wrefs(0).id, 'untitleddoc.p.3.s.9.w.7' , testmessage + "Testing order (1/3)", testresult)
+            testresult, testmessage = testequal(doc['untitleddoc.p.3.s.9.entity.1'].wrefs(1).id, 'untitleddoc.p.3.s.9.w.8' , testmessage + "Testing order (2/3)", testresult)
+            testresult, testmessage = testequal(doc['untitleddoc.p.3.s.9.entity.1'].wrefs(2).id, 'untitleddoc.p.3.s.9.w.9' , testmessage + "Testing order (3/3)", testresult)
+        elif testname in ( "newoverlapspan", "correction_newoverlapspan"):
+            testresult, testmessage = testequal(len(doc['untitleddoc.p.3.s.9.entity.1'].wrefs()),2, testmessage + "Testing original span size", testresult)
+            testresult, testmessage = testequal(doc['untitleddoc.p.3.s.9.entity.1'].wrefs(0).id, 'untitleddoc.p.3.s.9.w.8' , testmessage + "Testing original entity", testresult)
+            testresult, testmessage = testequal(len(doc['untitleddoc.p.3.s.9.entity.2'].wrefs()),3, testmessage + "Testing extra span size", testresult)
+            testresult, testmessage = testequal(doc['untitleddoc.p.3.s.9.entity.2'].wrefs(0).id, 'untitleddoc.p.3.s.9.w.7' , testmessage + "Testing extra entity", testresult)
+        elif testname in ( "spandeletion"):
+            testresult, testmessage = testequal('untitleddoc.p.3.s.9.entity.1' in doc,False, testmessage + "Testing absence of entity in index", testresult)
+        elif testname in ( "tokenannotationdeletion", "correction_tokenannotationdeletion"):
+            exceptionraised = False
+            try:
+                doc['untitleddoc.p.3.s.8.w.4'].lemma()
+            except folia.NoSuchAnnotation:
+                exceptionraised = True
+            testresult, testmessage = testequal(exceptionraised,True, testmessage + "Testing absence of lemma", testresult)
+        elif testname in  ("correction_worddelete"):
+            testresult, testmessage = testequal(doc['untitleddoc.p.3.s.8.correction.1'].original(0).id, 'untitleddoc.p.3.s.8.w.10',  testmessage + "Testing whether original word is now under original in correction", testresult)
+        elif testname in ( "correction_wordsplit"):
+            #entity ID will be different!
+            testresult, testmessage = testequal(doc['untitleddoc.p.3.s.12.correction.1'].original(0).id, 'untitleddoc.p.3.s.12.w.5',  testmessage + "Testing whether original word is now under original in correction", testresult)
+
+            testresult, testmessage = testequal(doc['untitleddoc.p.3.s.12.w.17'].text(),"4", testmessage + "Testing new word (1/2)", testresult)
+            testresult, testmessage = testequal(doc['untitleddoc.p.3.s.12.w.18'].text(),"uur", testmessage + "Testing new word (2/2)", testresult)
+            testresult, testmessage = testequal(doc['untitleddoc.p.3.s.12.w.17'].next().id,"untitleddoc.p.3.s.12.w.18", testmessage + "Testing order (1/2)", testresult)
+            testresult, testmessage = testequal(doc['untitleddoc.p.3.s.12.w.4'].next().id,"untitleddoc.p.3.s.12.w.17", testmessage + "Testing order (2/2)", testresult)
+        elif testname in ( "correction_wordinsertionright", "correction_wordinsertionleft"):
+            pass
+        elif testname in ("correction_spanchange"):
+            testresult, testmessage = testequal(doc['untitleddoc.p.3.s.9.correction.1'].original(0).id, 'untitleddoc.p.3.s.9.entity.1',  testmessage + "Testing whether original span is now under original in correction", testresult)
+            testresult, testmessage = testequal(len(doc['untitleddoc.p.3.s.9.entity.2'].wrefs()),3, testmessage + "Testing span size", testresult)
+            testresult, testmessage = testequal(doc['untitleddoc.p.3.s.9.entity.2'].wrefs(0).id, 'untitleddoc.p.3.s.9.w.7' , testmessage + "Testing order (1/3)", testresult)
+            testresult, testmessage = testequal(doc['untitleddoc.p.3.s.9.entity.2'].wrefs(1).id, 'untitleddoc.p.3.s.9.w.8' , testmessage + "Testing order (2/3)", testresult)
+            testresult, testmessage = testequal(doc['untitleddoc.p.3.s.9.entity.2'].wrefs(2).id, 'untitleddoc.p.3.s.9.w.9' , testmessage + "Testing order (3/3)", testresult)
+        elif testname in ( "correction_spandeletion"):
+            testresult, testmessage = testequal(doc['untitleddoc.p.3.s.9.correction.1'].original(0).id, 'untitleddoc.p.3.s.9.entity.1',  testmessage + "Testing whether original span is now under original in correction", testresult)
+        else:
+            testresult = False
+            testmessage += "No such test: " + testname
+    except Exception as e:
+            exc_type, exc_value, exc_traceback = sys.exc_info()
+            formatted_lines = traceback.format_exc().splitlines()
+            testresult = False
+            testmessage += "Test raised Exception in backend: " + str(e) + " -- " "\n".join(formatted_lines)
+
+
+    return (testresult, testmessage)
