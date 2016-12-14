1879d8b272223fd0c93eb3a5f89604c0fc82a93a
Maarten van Gompel <proycon@anaproy.nl>
2016-12-14 13:39:58 +0100

    Implemented spanrole deletion test (+ cleanup)

--- foliadocserve/test.py
+++ foliadocserve/test.py
@@ -241,6 +241,10 @@ def test(doc, testname, testmessage = ""):
         elif testname ==  "spanrole_respan":
             dependency = doc['untitleddoc.p.3.s.1.dependencies.1.dependency.10']
             testresult, testmessage = testequal(list(dependency.annotation(folia.Headspan).wrefs()), [ doc['untitleddoc.p.3.s.1.w.12'], doc['untitleddoc.p.3.s.1.w.12b'] ], testmessage + "Testing spanrole span", testresult )
+        elif testname ==  "spanrole_delete":
+            dependency = doc['untitleddoc.p.3.s.1.dependencies.1.dependency.10']
+            testresult, testmessage = testequal(len(dependency), 1 , testmessage + "Testing only one child remains", testresult )
+            testresult, testmessage = testequal(dependency[0].__class__, folia.DependencyDependent, testmessage + "Testing child is not the head", testresult )
         else:
             testresult = False
             testmessage += "No such test: " + testname
